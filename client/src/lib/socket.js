f573197201153e86aac53333d57f42f06752f480
Suhani <suhani05tyagi@gmail.com>
2025-08-22 21:59:42 +0530

    update socket.js

--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -1,13 +1,18 @@
+
 import { io } from "socket.io-client";
-
 const BASE_URL =
-  window.location.hostname === "localhost"
+  import.meta.env.VITE_BACKEND_URL ||
+  (typeof window !== "undefined" && window.location.hostname === "localhost"
     ? "http://localhost:8080"
-    : import.meta.env.VITE_BACKEND_URL;  
-
+    : "" 
+  );
 export const socket = io(BASE_URL, {
   path: "/socket.io",
   transports: ["websocket"],
   reconnection: true,
   withCredentials: true,
 });
+export function on(event, fn) {
+  socket.on(event, fn);
+  return () => socket.off(event, fn);
+}
